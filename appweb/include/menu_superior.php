d6f978ae9ddd07672f354978a928121b3dac86da
alchemist <159976232+full-metal-alchemist-dev@users.noreply.github.com>
2024-09-22 14:59:53 -0600

    Update menu_superior.php

--- appweb/include/menu_superior.php
+++ appweb/include/menu_superior.php
@@ -43,7 +43,7 @@
     <li class="nav-item dropdown user-menu">
       <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
         <img src="../dist/img/user2-160x160.jpg" class="user-image img-circle elevation-2" alt="User Image">
-        <span class="d-none d-md-inline"><?php echo $user['nombres'].' '.$user['apellidos']; ?></span>
+        <span class="d-none d-md-inline"><?php //echo $user['nombres'].' '.$user['apellidos']; ?></span>
       </a>
       <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
         <!-- User image -->
