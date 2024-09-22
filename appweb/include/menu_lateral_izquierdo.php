8d812fea1e580f7a88dfb021e63f5274b602d7c5
alchemist <159976232+full-metal-alchemist-dev@users.noreply.github.com>
2024-09-22 15:02:46 -0600

    Update menu_lateral_izquierdo.php

--- appweb/include/menu_lateral_izquierdo.php
+++ appweb/include/menu_lateral_izquierdo.php
@@ -4,7 +4,7 @@
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
       <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light"><?php echo $negocio; ?></span>
+      <span class="brand-text font-weight-light"><?php //echo $negocio; ?></span>
     </a>
 
     <!-- Sidebar -->
@@ -15,7 +15,7 @@
           <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?php echo $user['nombres'].' '.$user['apellidos']; ?></a>
+          <a href="#" class="d-block"><?php //echo $user['nombres'].' '.$user['apellidos']; ?></a>
         </div>
       </div>
 
