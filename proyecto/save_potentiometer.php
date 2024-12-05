333e209d7a93614e0ad7ce054ef7c9c54e1e8afb
MrFideo17 <fjchavarria91@gmail.com>
2024-12-04 20:35:52 -0600

    Update save_potentiometer.php

--- proyecto/save_potentiometer.php
+++ proyecto/save_potentiometer.php
@@ -1,17 +1,19 @@
 <?php
 
-
 require 'db_config.php';
 
-$potValue = $_GET['potValue'];  // Potentiometer value as a decimal
+
+$potValue = $_GET['voltage'];  // Potentiometer value as a decimal
+
+$pin = $_GET['pin']; 
 
 // Debugging: Log received value
 file_put_contents('log.txt', "Received potentiometer value: " . $potValue . "\n", FILE_APPEND);
 
 // Prepare and execute SQL to insert potentiometer value into the database
-$sql = "INSERT INTO potentiometer_data (pot_value, recorded_at) VALUES (?, NOW())";
+$sql = "INSERT INTO potentiometer_data (pot_value, recorded_at,pin) VALUES (?, NOW(), ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("d", $potValue);  // Bind the potentiometer value as a double (decimal)
+$stmt->bind_param("di", $potValue,$pin);  // Bind the potentiometer value as a double (decimal)
 $stmt->execute();
 
 // Check for errors
